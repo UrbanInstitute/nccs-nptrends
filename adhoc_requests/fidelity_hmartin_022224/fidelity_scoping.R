fb479876b2d7a6402db0b0dd9d34adb052385375
Thiyaghessan <thiyaghessan@gmail.com>
2024-02-26 09:11:33 -0500

    Temporary folder for adhoc data requests

---
+++ adhoc_requests/fidelity_hmartin_022224/fidelity_scoping.R
@@ -0,0 +1,354 @@
+rm(list = ls())
+
+source("C:/Users/tpoongundranar/Documents/Urban/NCCS/adhoc_requests/fidelity_hmartin_022224/fidelity_scoping_helpers.R")
+setwd("Y:/CNP/Generosity Commission/")
+
+library(readr)
+library(dplyr)
+library(rio)
+
+# Global Variables
+
+# Target states in Mountain West and South Central
+SCMW_STATES_ABBR <- scan(
+  text='Alabama
+        Arizona
+        Arkansas
+        Colorado
+        Idaho
+        Kentucky
+        Louisiana
+        Mississippi
+        Montana
+        Nevada
+        New Mexico
+        Oklahoma
+        Tennessee
+        Texas
+        Utah
+        Wyoming
+        ',
+  what = "", 
+  sep=",", 
+  quiet=TRUE, 
+  strip.white=TRUE
+) |> usdata::state2abbr()
+
+SC_STATES_ABBR <- scan(
+  text = 'Arizona 
+  Colorado 
+  Idaho 
+  Montana 
+  Nevada 
+  New Mexico 
+  Utah 
+  Wyoming',
+  what = "", 
+  sep=",", 
+  quiet=TRUE, 
+  strip.white=TRUE
+) |> usdata::state2abbr()
+
+MW_STATES_ABBR <- scan(
+  text = 'Alabama 
+  Arkansas 
+  Kentucky 
+  Louisiana 
+  Mississippi 
+  Oklahoma 
+  Tennessee 
+  Texas',
+  what = "", 
+  sep=",", 
+  quiet=TRUE, 
+  strip.white=TRUE
+) |> usdata::state2abbr()
+
+VARS_YEAR1_TARGET <- scan(
+  text = "DonImportance VolImportance FRchanges_1 FRchanges_2 FRchanges_3 FRchanges_2_1 FRchanges_2_2
+FRchanges_2_3 Funding2_1_5_1 CEOrace BCrace",
+  what = "",
+  sep = "",
+  quiet=TRUE,
+  strip.white=TRUE
+)
+
+VARS_YEAR2_TARGET <- scan(
+  text = "DonImportance
+VolImportance
+FRchanges_1
+FRchanges_2
+FRchanges_3
+FRchanges_4
+FRchanges_5
+FRchanges_6
+FRchanges_7
+FRchanges_8
+FRchanges_9
+FRchanges_1_1
+FRchanges_1_2
+FRchanges_1_3
+FRchanges_1_6
+FRchanges_1_7
+Funding1_1_9
+Funding1_2_9
+CEOrace_1
+CEOrace_2
+CEOrace_3
+CEOrace_4
+CEOrace_5
+CEOrace_6
+CEOrace_7
+BCrace_1
+BCrace_2
+BCrace_3
+BCrace_4
+BCrace_5
+BCrace_6
+BCrace_7
+",
+  what = "",
+  sep = "",
+  quiet=TRUE,
+  strip.white=TRUE
+)
+
+
+# Load in Survey data
+
+year1_raw <- readr::read_csv("Qualtrics Survey/Intermediate/gc_survey_27apr_21.csv")
+year2_raw <- rio::import("Qualtrics Survey/Intermediate/nptrends_year2_13jan23_with_y2_weight.sav")
+
+# Filter for target sates: South Central and Mountain West = SCMW
+
+year1_SCMWregion <- year1_raw %>% 
+  dplyr::filter(state %in% SCMW_STATES_ABBR)
+
+year1_SCMWregion_varcounts_df <- year1_SCMWregion |>
+  get_multi_variable_count(
+    variables = VARS_YEAR1_TARGET,
+    year = 1,
+    region = "Southwest and Mountain Central"
+  )
+
+year1_MWregion_varcounts_df <- year1_SCMWregion %>% 
+  dplyr::filter(state %in% MW_STATES_ABBR) |>
+  get_multi_variable_count(
+    variables = VARS_YEAR1_TARGET,
+    year = 1,
+    region = "Southwest"
+  )
+
+year1_SCregion_varcounts_df <- year1_SCMWregion %>% 
+  dplyr::filter(state %in% SC_STATES_ABBR) |>
+  get_multi_variable_count(
+    variables = VARS_YEAR1_TARGET,
+    year = 1,
+    region = "Mountain Central"
+  )
+
+# Combine Year 1 Results
+year1_varcounts_df <- purrr::list_rbind(
+  list(
+    year1_SCMWregion_varcounts_df,
+    year1_SCregion_varcounts_df,
+    year1_MWregion_varcounts_df
+  )
+)
+
+# Unique EINs for Southwest and Mountain Central Regions
+ein_SCMWregion <- unique(year1_SCMWregion$EIN)
+ein_MWregion <- year1_SCMWregion %>% 
+  dplyr::filter(state %in% MW_STATES_ABBR) %>% 
+  dplyr::pull("EIN")
+ein_SCregion <- year1_SCMWregion %>% 
+  dplyr::filter(state %in% SC_STATES_ABBR) %>% 
+  dplyr::pull("EIN")
+
+# Year 2 data sets
+year2_SCMWregion <- year2_raw %>% 
+  dplyr::filter(ein %in% ein_SCMWregion)
+
+year2_SCregion <- year2_raw %>% 
+  dplyr::filter(ein %in% ein_SCregion)
+
+year2_MWregion <- year2_raw %>% 
+  dplyr::filter(ein %in% ein_MWregion)
+
+# Process year 2 data
+
+# Board race columns
+race_board_cols <- c(
+  "BCrace_1",
+  "BCrace_2",
+  "BCrace_3",
+  "BCrace_4",
+  "BCrace_5",
+  "BCrace_7"
+)
+
+# CEO race columns
+race_ceo_cols <- c(
+  "CEOrace_1",
+  "CEOrace_2",
+  "CEOrace_3",
+  "CEOrace_4",
+  "CEOrace_5",
+  "CEOrace_7"
+)
+
+# EINs that have not changed board members in Year 2 Survey
+ein_brace_chng <- year2_SCMWregion %>% 
+  dplyr::filter( LeadershipChanges_4 != 1 | LeadershipChanges_5 != 1) %>% 
+  dplyr::pull("ein")
+
+# EINs that have not changed CEOs in Year 2 Survey
+ein_ceorace_chng <- year2_SCMWregion %>% 
+  dplyr::filter( LeadershipChanges_6 != 1) %>% 
+  dplyr::pull("ein")
+
+# Get Survey responses for race of Board members from Year 1 for participants
+# in year 2 that reported no change
+year1_SCMWregion_brace_chng <- year1_SCMWregion %>% 
+  dplyr::filter(EIN %in% ein_brace_chng,
+                ! BCrace %in% c(-99, NA)) %>% 
+  dplyr::select(state, BCrace) %>% 
+  dplyr::mutate(BCrace = paste0("BCrace_", BCrace)) %>%
+  dplyr::group_by(state, BCrace) %>% 
+  dplyr::summarise("Number of Responses" = n()) %>% 
+  dplyr::rename("Variable Name" = "BCrace")
+
+year1_SCregion_brace_chng <- year1_SCMWregion_brace_chng %>% 
+  dplyr::filter(state %in% SC_STATES_ABBR)
+
+year1_MWregion_brace_chng <- year1_SCMWregion_brace_chng %>% 
+  dplyr::filter(state %in% MW_STATES_ABBR)
+
+# Get Survey responses for race of CEOs from Year 1 for participants
+# in year 2 that reported no change
+year1_SCMWregion_ceorace_chng <- year1_SCMWregion %>% 
+  dplyr::filter(EIN %in% ein_ceorace_chng,
+                ! CEOrace %in% c(-99, NA)) %>% 
+  dplyr::select(state, CEOrace) %>% 
+  dplyr::mutate(CEOrace = paste0("CEOrace_", CEOrace)) %>%
+  dplyr::group_by(state, CEOrace) %>% 
+  dplyr::summarise("Number of Responses" = n()) %>% 
+  dplyr::rename("Variable Name" = "CEOrace")
+
+year1_SCregion_ceorace_chng <- year1_SCMWregion_ceorace_chng %>% 
+  dplyr::filter(state %in% SC_STATES_ABBR)
+
+year1_MWregion_ceorace_chng <- year1_SCMWregion_ceorace_chng %>% 
+  dplyr::filter(state %in% MW_STATES_ABBR)
+
+# Recode Board Member and CEO race variables in Year 2 Survey data for EINs
+# to include a category for bi-racial classifications
+year2_SCMWregion_proc <- year2_SCMWregion %>%
+  dplyr::mutate(BCrace_6 = ifelse(rowSums(dplyr::across(race_board_cols)) > 1,
+                                  1,
+                                  NA),
+                CEOrace_6 = ifelse(rowSums(dplyr::across(race_ceo_cols)) > 1,
+                                   1,
+                                   NA)) %>% 
+  dplyr::mutate_at(race_board_cols, ~ ifelse(BCrace_6 == 1, NA, .),
+                   race_ceo_cols, ~ ifelse(CEOrace_6 == 1, NA, .),
+                   c(race_board_cols, race_ceo_cols), ~ ifelse(. == 0, NA, .)) 
+
+year2_SCregion_proc <- year2_SCMWregion_proc %>% 
+  dplyr::filter(ein %in% ein_SCregion)
+
+year2_MWregion_proc <- year2_SCMWregion_proc %>% 
+  dplyr::filter(ein %in% ein_MWregion)
+
+
+# Get counts of responses for year 2 survey participants
+year2_SCMWregion_responses <- get_multi_variable_count(
+  variables = VARS_YEAR2_TARGET,
+  data = year2_SCMWregion_proc,
+  year = 2,
+  region = "South Central and Mountain West"
+)
+
+year2_SCregion_responses <- get_multi_variable_count(
+  variables = VARS_YEAR2_TARGET,
+  data = year2_SCregion_proc,
+  year = 2,
+  region = "South Central"
+)
+
+year2_MWregion_responses <- get_multi_variable_count(
+  variables = VARS_YEAR2_TARGET,
+  data = year2_MWregion_proc,
+  year = 2,
+  region = "Mountain West"
+)
+
+# Get counts of responses including year 1 responses for race when CEO/Board
+# Member race in Year 2 does not change
+year2_SCMWregion_varcounts_df <- purrr::list_rbind(
+  list(
+    year2_SCMWregion_responses, 
+    year1_SCMWregion_brace_chng %>% 
+      dplyr::select(!state),
+    year1_SCMWregion_ceorace_chng %>% 
+      dplyr::select(!state)
+  )
+) %>% 
+  dplyr::group_by(`Variable Name`) %>% 
+  dplyr::summarise("Number of Responses" = sum(`Number of Responses`))
+
+year2_SCregion_varcounts_df <- purrr::list_rbind(
+  list(
+    year2_SCregion_responses, 
+    year1_SCregion_brace_chng %>% 
+      dplyr::select(!state),
+    year1_SCregion_ceorace_chng %>% 
+      dplyr::select(!state)
+  )
+) %>% 
+  dplyr::group_by(`Variable Name`) %>% 
+  dplyr::summarise("Number of Responses" = sum(`Number of Responses`))
+
+year2_MWregion_varcounts_df <- purrr::list_rbind(
+  list(
+    year2_MWregion_responses, 
+    year1_MWregion_brace_chng %>% 
+      dplyr::select(!state),
+    year1_MWregion_ceorace_chng %>% 
+      dplyr::select(!state)
+  )
+) %>% 
+  dplyr::group_by(`Variable Name`) %>% 
+  dplyr::summarise("Number of Responses" = sum(`Number of Responses`))
+
+
+year2_varcounts_df <- purrr::list_rbind(
+  list(
+    year2_SCMWregion_varcounts_df %>% 
+      dplyr::mutate(Year = "2",
+                    Region = "Southwest and Mountain Central"),
+    year2_SCregion_varcounts_df  %>% 
+      dplyr::mutate(Year = "2",
+                    Region = "Southwest"),
+    year2_MWregion_varcounts_df  %>% 
+      dplyr::mutate(Year = "2",
+                    Region = "Mountain Central")
+  )
+)
+year2_varcounts_df
+
+# Combine all results together
+
+varcounts_df <- purrr::list_rbind(
+  list(
+    year1_varcounts_df,
+    year2_varcounts_df
+  )
+)
+
+View(varcounts_df)
+
+write.csv(
+  varcounts_df,
+  "C:/Users/tpoongundranar/Documents/Urban/NCCS/adhoc_requests/fidelity_hmartin_022224/scmw_varcounts.csv"
+)
