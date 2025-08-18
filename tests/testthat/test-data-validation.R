c5a5050698534d5e8ec74d350983f3fc62c3492c
Thiyaghessan <thiyaghessan@gmail.com>
2025-08-18 19:17:09 -0400

    Refactor code and add additional tests to ensure computations and metric numbering are correct

--- tests/testthat/test-data-validation.R
+++ tests/testthat/test-data-validation.R
@@ -1,17 +1,19 @@
+# Unit testing to ensure output data matches data template
+
 # libraries
 library(testthat)
 library(data.table)
 library(tidyverse)
 
+# Datasets used across multiplte testthat functions
+root <- "../../"
+nptrends_full_postprocessed <- data.table::fread(
+  paste0(root, nptrends_full_postproc_path)
+)
+template <- data.table::fread(paste0(root, template_path))
+
+# Tests
 testthat::test_that("All permutations in template present", {
-  # Load in template
-  root <- "../../"
-  template <- data.table::fread(
-    paste0(root, template_path)
-  )
-  nptrends_full_postprocessed <- data.table::fread(
-    paste0(root, nptrends_full_postproc_path)
-  )
   combinations_validate_df <- data.table::fread(
     paste0(root, combinations_validate_path)
   )
@@ -28,4 +30,50 @@ testthat::test_that("All permutations in template present", {
     data.table::setDT(postproc_combinations),
     data.table::setDT(combinations_validate_df)
   ))
+})
+
+testthat::test_that("Values were computed correctly", {
+  # Values computed using Hannah's scripts in Y:\CNP\Generosity Commission\Year 5\Analyses\Dashboard Checking
+  validation_df <- data.table::fread("../../data/validate/validate-value.csv", 
+                                     na.strings = "")
+  validation_df <- validation_df |>
+    dplyr::mutate(key = paste0(metricID, 
+                               responseOpt, 
+                               splitByOpt_category, 
+                               year)) |>
+    dplyr::select(key, value) |>
+    arrange(key)
+  postproc_validate <- nptrends_full_postprocessed |>
+    dplyr::filter(metricID %in% c(1, 4, 62),
+                  filterType == "National",
+                  filterOpt == "National") |>
+    dplyr::select(
+      metricID,
+      responseOpt,
+      splitByOpt_category,
+      year,
+      value
+    ) |>
+    unique() |>
+    dplyr::mutate(
+      year = as.character(year),
+      metricID = as.integer(metricID)
+    ) |>
+    dplyr::mutate(key = paste0(metricID, 
+                               responseOpt, 
+                               splitByOpt_category, 
+                               year)) |>
+    dplyr::select(key, value) |>
+    dplyr::arrange(key)
+  testthat::expect_equal(validation_df, postproc_validate, tolerance = 0.02)
+})
+
+testthat::test_that("Metrics are numbered and ordered correctly", {
+  templateids <- template |>
+    dplyr::select(metricID, responseOpt) |>
+    unique()
+  myids <- nptrends_full_postprocessed |>
+    dplyr::select(metricID, responseOpt) |>
+    unique()
+  testthat::expect_equal(templateids, myids)
 })
