64759fb41ca260aec0f757e2236dd3130abae75c
Thiyaghessan <thiyaghessan@gmail.com>
2025-08-26 20:20:18 -0400

    Update metadata to rely on internal R objects and avoid excessive pre-processing of validation datasets'

--- R/metadata.R
+++ R/metadata.R
@@ -1,3 +1,106 @@
+# Setup googlesheets
+library(googlesheets4)
+
+# googlesheets4::gs4_auth(email = "your_google_email@gmail.com",
+#                         scopes = "https://www.googleapis.com/auth/spreadsheets")
+
+# metricsMaster table for validation
+metricsMaster <- googlesheets4::read_sheet(metricsMaster_gsheet_url)
+
+# lookup table mapping variable names in the survey to metricID values
+metricID_lookup <- tibble::tribble(
+  ~metricID, ~metricname,
+  1, "TotRev_clean",
+  2, "TotExp",
+  3, "Reserves_Est",
+  4, "FinanceChng_Reserves",
+  5, "FinanceChng_TotExp",
+  6, "FinanceChng_Salaries",
+  7, "FinanceChng_Benefits",
+  8, "FinanceChng_TotRent",
+  9, "FinanceChng_TotTech",
+  10, "GeoAreas_ServedLocal",
+  11, "GeoAreas_State",
+  12, "GeoAreas_Servedmultistate",
+  13, "GeoAreas_National",
+  14, "GeoAreas_International",
+  15, "ProgDem_BelowFPL",
+  16, "ProgDem_Disabled",
+  17, "ProgDem_Veterans",
+  18, "ProgDem_LGBTQ",
+  19, "ProgDem_Foreign",
+  20, "ProgDem_Latinx",
+  21, "ProgDem_Black",
+  22, "ProgDem_Indigenous",
+  23, "ProgDem_Asian",
+  24, "ProgDem_Men",
+  25, "ProgDem_Women",
+  26, "ProgDem_Nonbinary",
+  27, "ProgDem_Children",
+  28, "ProgDem_YoungAdults",
+  29, "ProgDem_Adults",
+  30, "ProgDem_Elders",
+  31, "PplSrv_NumWait",
+  32, "PrgSrvc_Suspend",
+  33, "PrgSrvc_Amt_Srvc",
+  34, "PrgSrvc_Amt_Num",
+  35, "Dmnd_NxtYear",
+  36, "PercentDon_Tot",
+  37, "FndRaise_DnrBlw250_ratio",
+  38, "FndRaise_DnrAbv250_ratio",
+  39, "PercentDAF_Tot",
+  40, "PercentPriv_Tot",
+  41, "PercentCorp_Tot",
+  42, "PercentGov",
+  43, "PercentEarned",
+  44, "FndRaise_DAF_Rcv",
+  45, "FndRaise_PFGrnt_Rcv",
+  46, "FndRaise_Corp_Found_Grnt_Rcv",
+  47, "FndRaise_CFGrnt_Rcv",
+  48, "GovFunding",
+  49, "CARES_Rcv",
+  50, "FndRaise_DAF_Grnt_Chng",
+  51, "FndRaise_Priv_Grnt_Chng",
+  52, "FndRaise_Corp_Grnt_Chng",
+  53, "PrgSrvc_Amt_Fee",
+  54, "FndRaise_TotExp",
+  55, "FndRaise_TotDigAppeal",
+  56, "FndRaise_TotDirMail",
+  57, "FndRaise_TotVirtEvent",
+  58, "FndRaise_TotInPersEvent",
+  59, "PercentDem_Women_Staff",
+  60, "Staff_RegVlntr",
+  61, "Staff_EpsdVlntr",
+  62, "PercentDem_Women_Board",
+  63, "PercentDem_LGBTQ_Board",
+  64, "PercentDem_Disabled_Board",
+  65, "PercentDem_Young_Board",
+  66, "PercentDem_ReceivedServices_Board",
+  67, "PercentDem_POC_Staff",
+  68, "PercentDem_LGBTQ_Staff",
+  69, "PercentDem_Disabled_Staff",
+  70, "PercentDem_Young_Staff",
+  71, "PercentDem_ReceivedServices_Staff",
+  72, "Staff_Boardmmbr",
+  73, "PercentDem_POC_Board",
+  74, "CEOrace_POC",
+  75, "Dem_CEO_LGBTQ",
+  76, "Dem_CEO_Disabled",
+  77, "Dem_CEO_Under35",
+  78, "BChair_POC",
+  79, "Dem_BChair_LGBTQ",
+  80, "Dem_BChair_Disabled",
+  81, "Dem_BChair_Under35",
+  82, "Staff_Fulltime",
+  83, "Staff_Parttime",
+  84, "CEOgender_Man",
+  85, "CEOgender_Woman",
+  86, "CEOgender_NB",
+  87, "BChairgender_Man",
+  88, "BChairgender_Woman",
+  89, "BChairgender_NB"
+)
+
 # lookup table to recode responseOpt for continuous variables
 responseOpt_lookup <- tibble::tribble(
   ~responseOpt_lookup, ~metricname,
@@ -10,23 +113,11 @@ responseOpt_lookup <- tibble::tribble(
   "Donor advised funds", "PercentDAF_Tot",
   "Private foundations", "PercentPriv_Tot",
   "Corporate foundations or giving programs", "PercentCorp_Tot",
-  "Gov't", "PercentGov",
+  "Government", "PercentGov",
   "Earned Revenue", "PercentEarned",
-  "Women", "PercentDem_Women_Board",
   "Regular volunteers", "Staff_RegVlntr",
   "Episodic volunteers", "Staff_EpsdVlntr",
-  "Women", "PercentDem_Women_Staff",
-  "LGBTQ+", "PercentDem_LGBTQ_Board",
-  "Disabled", "PercentDem_Disabled_Board",
-  "Under 35 years old", "PercentDem_Young_Board",
-  "Past or current recipient of nonprofits' services", "PercentDem_ReceivedServices_Board",
-  "Person of color", "PercentDem_POC_Staff",
-  "LGBTQ+", "PercentDem_LGBTQ_Staff",
-  "Disabled", "PercentDem_Disabled_Staff",
-  "Under 35 years old", "PercentDem_Young_Staff",
-  "Past or current recipient of nonprofits' services", "PercentDem_ReceivedServices_Staff",
-  "Number of board members", "Staff_Boardmmbr",
-  "Person of color", "PercentDem_POC_Board"
+  "Number of board members", "Staff_Boardmmbr"
 )
 
 # Template containing desired outputs, excluding values for survey metrics
@@ -42,8 +133,7 @@ combinations_validate_df <- template |>
       splitByOpt_category == "$500,000-$999,999" ~ "$500,000 - $999,999",
       splitByOpt_category == "$1 million-$9,999,999" ~ "$1 million - $9,999,999",
       .default = splitByOpt_category
-    ),
-    responseOpt = tolower(gsub("’", "'", responseOpt))
+    )
   ) |>
   dplyr::select(filterType,
                 filterOpt,
@@ -54,9 +144,12 @@ combinations_validate_df <- template |>
 data.table::fwrite(combinations_validate_df, combinations_validate_path)
 
 # Metadata to merge with postprocessed data to resemble data template
-template_metadata <- data.table::fread(template_metadata_path) |>
+template_metadata <- metricsMaster |>
+  tidylog::left_join(metricID_lookup, by = "metricID") |>
+  dplyr::mutate(metricID = as.integer(metricID)) |>
   dplyr::select(metricID,
                 theme,
                 category,
                 dataVizType,
-                metricname)
+                metricname) |>
+  as.data.frame()
