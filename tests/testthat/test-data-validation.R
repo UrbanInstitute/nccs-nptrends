e0530f3d0825ac177ebcaab8104bc89bb3fc57dd
Thiyaghessan <thiyaghessan@gmail.com>
2025-11-05 16:36:46 -0500

    Update unit tests to account for changes made to data template

--- tests/testthat/test-data-validation.R
+++ tests/testthat/test-data-validation.R
@@ -43,12 +43,14 @@ testthat::test_that("Correct states have been excluded", {
   testthat::expect_false(any(states_present_2025 %in% excluded_2025))
 })
 
+# We filter responseOpt because the final dataset does not have the binary == 0 values (e.g. has "Serving seniors" but not "Not serving seniors")
 testthat::test_that("All permutations in template present", {
   combinations_validate_df <- data.table::fread(
     paste0(root, combinations_validate_path)
   )
   
   formatted_combinations <- nptrends_full_formatted |>
+    dplyr::filter(responseOpt %in% combinations_validate_df$responseOpt) |>
     dplyr::select(filterType,
                   filterOpt,
                   splitByOpt,
@@ -66,15 +68,13 @@ testthat::test_that("Values were computed correctly", {
   validation_df <- data.table::fread("../../data/validate/validate-value.csv", 
                                      na.strings = "")
   validation_df <- validation_df |>
-    dplyr::mutate(splitByOpt = ifelse(is.na(splitByOpt), "", splitByOpt),
-                  splitByOpt_category = ifelse(is.na(splitByOpt_category), 
-                                               "", 
-                                               splitByOpt_category),
-                  responseOpt = dplyr::case_when(
-                    responseOpt == "Oct-49" ~ "10-49",
-                    responseOpt == "9-Feb" ~ "2-9",
-                    .default = responseOpt
-                  )) |>
+    dplyr::filter(metricID %in% c(1, 5, 36)) |>
+    dplyr::mutate(
+      splitByOpt = ifelse(is.na(splitByOpt), "", splitByOpt),
+      splitByOpt_category = ifelse(is.na(splitByOpt_category), 
+                                   "", 
+                                   splitByOpt_category)
+    ) |>
     dplyr::mutate(key = paste0(metricID,
                                year,
                                filterType,
@@ -108,21 +108,19 @@ testthat::test_that("Metrics are numbered and ordered correctly", {
     dplyr::arrange(responseOpt)
   myids <- nptrends_full_filtered |>
     dplyr::select(metricID, responseOpt) |>
+    dplyr::filter(responseOpt %in% templateids$responseOpt) |>
     unique()  |>
     dplyr::arrange(responseOpt)
   testthat::expect_equal(templateids, myids)
 })
 
+# We expect a subset because the final dataset does not have the binary == 0 values (e.g. has "Serving seniors" but not "Not serving seniors")
 testthat::test_that("ResponseOpt values are formatted correctly", {
   myresponseOpt <- nptrends_full_filtered |>
-    dplyr::pull(responseOpt) |>
-    unique() |>
-    sort()
+    dplyr::pull(responseOpt)
   templateresponseOpt <- template |>
-    dplyr::pull(responseOpt) |>
-    unique() |>
-    sort()
-  testthat::expect_equal(myresponseOpt, templateresponseOpt)
+    dplyr::pull(responseOpt)
+  testthat::expect_true(all(templateresponseOpt %in% myresponseOpt))
 })
 
 testthat::test_that("Correct column names are present", {
